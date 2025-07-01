2b83a6bd097d06b0d3418fbf41aab58646561f3d
korea300016 <korea300016@gmail.com>
2025-07-01 17:40:22 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0A2342",
+        secondary: "#FFD700"
+      },
+      fontFamily: {
+        sans: ['Noto Sans KR', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+} 
