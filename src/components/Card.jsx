21ecad281758fa7bd0ccb913bf1b373192a48042
korea300016 <korea300016@gmail.com>
2026-04-14 11:56:54 +0900

    Delete src directory

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Card({ children }) {
-  return (
-    <div className="bg-white shadow-md rounded-xl p-6 mb-6 border border-gray-100">
-      {children}
-    </div>
-  );
-} 
